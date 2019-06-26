818d007229b35e92fa246f25d02e74173a86bb30
glorianaa80 <glorianaa80@gmail.com>
2019-06-26 16:47:09 -0600

    Cambios

---
+++ backend/controllers/cars.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const carsJson = require('../inventories/cars.json');
+
+const {
+  end,
+  getData,
+  throwErr
+} = require('./method');
+
+
+function GET(req, res){
+
+}
+
+function POST(req, res){
+
+}
+
+function PUT(req, res){
+
+}
+
+function DELEDE(req, res){
+   
+}
+
+
+
+module.exports = {
+  GET,
+  POST,
+  PUT,
+  DELEDE
+};
